25442e43350ca7b322227dd4772595da1916dc71
Thejas B <thejasb@admins-MacBook-Pro.local>
2018-07-09 10:33:47 +0530

    Temp

--- main.go
+++ main.go
@@ -6,14 +6,27 @@ import
     "net/http"
     "github.com/gorilla/mux"
     "github.com/thejasbabu/userManagement/handler"
+    "github.com/kelseyhightower/envconfig"
 )
 
+type Config struct {
+    AppPort           int
+    MgDbURL           string
+    MgDbName          string
+    UserMgCollection  string
+}
+
 func main() {
+    var config Config
+    err := envconfig.Process("UM", &config)
+    if err != nil {
+      log.Fatal(err.Error())
+    }
     router := mux.NewRouter()
     userHandler := handler.NewUserHandler()
     router.HandleFunc("/user", userHandler.GetUsers).Methods("GET")
     router.HandleFunc("/user/{id}", userHandler.GetUser).Methods("GET")
     router.HandleFunc("/user", userHandler.CreateUser).Methods("POST")
     router.HandleFunc("/user/{id}", userHandler.DeleteUser).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8000", router))
+    log.Fatal(http.ListenAndServe(config.AppPort, router))
 }
